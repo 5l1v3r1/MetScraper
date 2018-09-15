d142e257fdca8fe12b620d59d09d3490b30fb2b0
GingerBeardx <ecryptographer@gmail.com>
2018-09-15 09:25:37 -0600

    scraped data and looped over 5 sets excluding videos

--- metscrape.py
+++ metscrape.py
@@ -4,19 +4,28 @@ from bs4 import BeautifulSoup
 import requests
 
 # opens the file that we've saved and turns it into soup
-with open('18.9.14 page.html') as html_file:
+with open('18.9.15 page.html') as html_file:
     soup = BeautifulSoup(html_file, 'lxml')
 
-# finds the first set and displys info in the console
-set_info = soup.find('div', class_='custom-list-item-detailed-photo-stats')
-print(set_info)
+# Loop through 5 sets and display the info to the console
+looper = 0
+for set_info in soup.findAll('div', class_='custom-list-item-detailed-photo-stats'):
+    if looper < 5:
+        set_meta = set_info.findAll('span', class_="custom-age")
+        if len(set_meta) < 3:
+            continue
+        else:
+            # print(set_info)
 
-set_name = set_info.h3.text
-set_url = set_info.h3.a['href']
-print(set_name)
-print(set_url)
+            set_name = set_info.h3.text
+            set_url = set_info.h3.a['href']
+            print(set_name)
+            print(set_url)
 
-set_meta = set_info.findAll('span', class_="custom-age")
-print(len(set_meta))
-for data_point in set_meta:
-    print(data_point.text)
+            for data_point in set_meta:
+                print(data_point.text)
+            
+            print("-" * 100)
+        looper += 1
+    else:
+        break
